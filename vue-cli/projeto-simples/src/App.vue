c45ceb22860738ea6918cab0a9cac1c0f7f9d56b
Wallace Machado dos Santo Melo <walace.melo@el.com.br>
2023-10-16 11:25:55 -0300

    aula de estruturas e componentes

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>{{ titulo }}</h1>
+    <button @click="titulo += '#'">adicionar #</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      titulo: "Teste data usando Vue.js!!",
+    };
+  },
+};
+</script>
+
+<style>
+#app{
+  background-color: chocolate;
+  color: white;
+}
+</style>
