6cca11f3ec87b492c55d458ab9d21dc0ae677fd1
Wallace Machado dos Santo Melo <walace.melo@el.com.br>
2023-10-18 16:45:37 -0300

    compomente comunicação atualização

--- componentes/componentes-comunicacao/src/components/Usuario.vue
+++ componentes/componentes-comunicacao/src/components/Usuario.vue
@@ -2,39 +2,50 @@
     <div class="container">
         <h1>Componente Usuário</h1>
         <p>Esse é um componente muito legal!</p>
+        <button @click="alterarNome">Alterar nome</button>
         <hr>
         <div class="componentes">
-            <app-usuario-info />
+            <app-usuario-info :nome="nome" />
             <app-usuario-editar />
         </div>
     </div>
 </template>
 
 <script>
-import AppUsuarioInfo from './UsuarioInfo'
-import AppUsuarioEditar from './UsuarioEditar'
+import AppUsuarioInfo from "./UsuarioInfo";
+import AppUsuarioEditar from "./UsuarioEditar";
 
 export default {
-    components: { AppUsuarioInfo, AppUsuarioEditar }
-}
+  components: { AppUsuarioInfo, AppUsuarioEditar },
+  data() {
+    return {
+      nome: "Pedro Silva",
+    };
+  },
+  methods: {
+    alterarNome() {
+      this.nome = "Ana Lopes";
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .container {
-        background-color: #27363b;
-        color: #fff;
-        padding: 10px;
-    }
+.container {
+  background-color: #27363b;
+  color: #fff;
+  padding: 10px;
+}
 
-    .container hr {
-        margin: 20px 10px;
-    }
+.container hr {
+  margin: 20px 10px;
+}
 
-    .componentes {
-        display: flex;
-    }
+.componentes {
+  display: flex;
+}
 
-    .componentes > * {
-        margin: 10px;
-    }
+.componentes > * {
+  margin: 10px;
+}
 </style>
