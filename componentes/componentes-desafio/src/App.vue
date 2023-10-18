ac374d9c6af604b086f2e632fddb7a7e33d62b3b
Wallace Machado dos Santo Melo <walace.melo@el.com.br>
2023-10-18 15:44:44 -0300

    organização de pastas componentes comunicação

---
+++ componentes/componentes-desafio/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <app-navigation /> 
+    <app-bar />
+    <app-carousel />
+    <app-footer />
+  </v-app>
+</template>
+<script>
+import nav from "./components/Navigation.vue";
+import bar from "./components/Bar.vue";
+import carousel from "./components/Carousel.vue";
+import footer from "./components/Footer.vue";
+
+export default {
+  /* components: { nav, bar,carousel, footer,}, ----- / Poderia ser utilzado assim tambem - neste modo não precisa colocar o nome da tag e a variavel, basta repetir o nome da variavel importada*/ 
+
+    /* components: {
+    "appNavigation": nav,  ----/ Poderia usar desta forma definida em camelCase e o vue entende o app-navigation , o correto seria usar tag appNavigation 
+    "appBar": bar,
+    "appCarousel": carousel,
+    "appFooter": footer,
+  }, */
+
+  components: {
+    "app-navigation": nav, 
+    "app-bar": bar,
+    "app-carousel": carousel,
+    "app-footer": footer,
+  },
+  
+
+};
+</script>
--- componentes/componentes-desafio/src/main.js
+++ componentes/componentes-desafio/src/main.js
@@ -1,16 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
-import nav from "./components/Navigation.vue";
+/* import nav from "./components/Navigation.vue";
 import bar from "./components/Bar.vue";
 import carousel from "./components/Carousel.vue";
-import footer from "./components/Footer.vue";
+import footer from "./components/Footer.vue"; */
 
 Vue.config.productionTip = false;
-Vue.component("app-navigation", nav);
+/* Vue.component("app-navigation", nav);
 Vue.component("app-bar", bar);
 Vue.component("app-carousel", carousel);
-Vue.component("app-footer", footer);
+Vue.component("app-footer", footer); */
 
 new Vue({
   vuetify,
